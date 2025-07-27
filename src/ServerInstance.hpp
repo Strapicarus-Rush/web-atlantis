df31e2e37a75f59087d63f77943739a83939f20f
strapicarus-rush <strapicarus@proton.me>
2025-07-27 01:59:10 -0400

    Bug Fix -Bug fix for start server when tmux session exist but server not running.

--- src/ServerInstance.hpp
+++ src/ServerInstance.hpp
@@ -59,7 +59,7 @@ public:
             return false;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(150));
-        
+        debug_log("is_running");
         std::string output;
         try {
             output = read_output();
@@ -114,11 +114,12 @@ public:
             if(is_running()){
                 return {true, message};
             }else{
-                cmd = file_to_run;
+                cmd = "tmux send-keys -t \"" + session + "\" "
+                    "\"cd \\\"" + path + "\\\" && " + file_to_run + "\"";
             }
         }else{
-            cmd = "tmux new-session -d -s " + session + " \""
-                    "cd " + path + " && " + file_to_run + "\"";
+            cmd = "tmux new-session -d -s \"" + session + "\" "
+                    "\"cd \\\"" + path + "\\\" && " + file_to_run + "\"";
         }
 
         int result = std::system(cmd.c_str());
